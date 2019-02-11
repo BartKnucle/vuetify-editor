af32b6fe7fb323f949d3ae73fd1abb59068ba193
BartKnucle <sjarde@yahoo.fr>
2019-02-11 21:30:53 +0100

    Added tree

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,23 @@
       <v-toolbar-title>
         <span>VUETIFY EDITOR</span>
       </v-toolbar-title>
-        <v-btn 
+      <v-btn 
           flat
           icon
-          @click="addComponent('v-btn')">
+          @click="addChild('v-btn')">
           <v-icon>add_box</v-icon>
-        </v-btn>
-      </v-toolbar>
+      </v-btn>
+    </v-toolbar>
     <v-content>
-       <template v-for="(child) in componentsTree">
-            <component :is="child" :key="child"></component>
-        </template>
+      <v-treeview
+        activatable
+        :active.sync="active"
+        :items="tree">
+      </v-treeview>
+      {{ active }}
+      <!-- <template v-for="(child) in tree">
+        <component :is="child" :key="child"></component>
+      </template> -->
     </v-content>
   </v-app>
 </template>
@@ -24,13 +30,45 @@ export default {
   components: {},
   data () {
     return {
-      componentsTree: []
+      active: [],
+      tree: [
+        {
+          id: 'root',
+          name: 'root',
+          children: []
+        }
+      ]
     }
   },
   methods: {
-    addComponent: function(name) {
-      this.componentsTree.push(name)
-    }
+    findNode(id, tree) {
+      tree.forEach(element => {
+        
+      });
+    },
+    addChild: function(type) {
+      if (this.active.length) {
+
+        var pushChild = function (id, tree) {
+          tree.forEach(element => {
+            if (element.id = id) {
+              element.children.push({
+                id: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+                name: type,
+                children: []
+              })
+            } else {
+              pushChild(id, element.children)
+            }
+          })
+        }
+        pushChild(this.active[0], this.tree)
+      }
+    },
+    activateNode: function() {}
+  },
+  watch: {
+    active: 'activateNode'
   }
 }
 </script>
