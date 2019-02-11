3e1626b8657bd9af293de63a0cd58487bc1a6609
BartKnucle <sjarde@yahoo.fr>
2019-02-11 18:17:36 +0100

    Dynamic component

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title>
+        <span>VUETIFY EDITOR</span>
+      </v-toolbar-title>
+        <v-btn 
+          flat
+          icon
+          @click="addComponent('v-btn')">
+          <v-icon>add_box</v-icon>
+        </v-btn>
+      </v-toolbar>
+    <v-content>
+       <template v-for="(child) in componentsTree">
+            <component :is="child" :key="child"></component>
+        </template>
+    </v-content>
+  </v-app>
+</template>
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data () {
+    return {
+      componentsTree: []
+    }
+  },
+  methods: {
+    addComponent: function(name) {
+      this.componentsTree.push(name)
+    }
+  }
+}
+</script>
